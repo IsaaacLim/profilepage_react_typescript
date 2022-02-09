1c881b44e23c21d2312c58c7c55c7babccf6b993
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-09 18:20:26 +0800

    added parallax

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./styles/css/style.css";
+import { Parallax, Background } from "react-parallax";
+import background from "./images/pear-dessert.jpg";
 
 const Home = () => {
   return (
     <div>
-      <h1>Isaac Lim</h1>
-      <p>On a mission to becoming a Software Developer,</p>
-      <p>with an interest in tasteful designs</p>
+      <Parallax strength={400}>
+        <Background className="bg">
+          <img src={background} alt="pear dessert" />
+        </Background>
+        <div className="main">
+          <h1>Isaac Lim</h1>
+          <div>
+            <p>On a mission to becoming a Software Developer,</p>
+            <p>with an interest in tasteful designs</p>
+          </div>
+        </div>
+      </Parallax>
+      <div style={{ height: "100vh" }}>something something</div>
     </div>
   );
 };
