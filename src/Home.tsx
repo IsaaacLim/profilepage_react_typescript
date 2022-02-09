07161bb2b19720b43e5c094a342c816d41902e1b
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-10 00:46:29 +0800

    removed react-parallex

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 import "./styles/css/style.css";
-import { Parallax, Background } from "react-parallax";
-import background from "./images/pear-dessert.jpg";
 import { HomeInfo } from "./components/HomeInfo";
 
 const Home = () => {
   return (
     <div>
-      <Parallax strength={400}>
-        <Background className="bg">
-          <img src={background} alt="pear dessert" />
-        </Background>
-        <div className="main">
-          <h1>Isaac Lim</h1>
-          <p>
-            On a mission to becoming a Software Developer, <br />
-            with an interest in tasteful designs
-          </p>
-        </div>
-      </Parallax>
+      <div className="main">
+        <h1>Isaac Lim</h1>
+        <p>
+          On a mission to becoming a Software Developer, <br />
+          with an interest in tasteful designs
+        </p>
+      </div>
       <HomeInfo />
     </div>
   );
