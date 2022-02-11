27c4f32c0fb7d0b0cf8fee6fe6c635368f65ffc0
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-12 02:01:09 +0800

    rough outline of About done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,63 @@
 import React from "react";
 import "../styles/css/style.css";
+import Image1 from "../images/greens.png";
+import Image2 from "../images/hot-air-balloon.png";
+import Image3 from "../images/mountains.png";
+import Image4 from "../images/world-tree.png";
+import Image5 from "../images/hourglass.png";
 
 export const About = () => {
   return (
-    <div className="home-info-cont">
-      <div className="page1">
-        <h2 className="header">
-          Making my way <br /> into the software development <br />
+    <div className="about-cont">
+      <div className="section" id="one">
+        <h2>
+          Making my way
+          <br />
+          into the software development
+          <br />
           industry.
         </h2>
-        <p className="text">
-          My name is Isaac and I'm a work in progress. While I'm still
-          <br /> gathering more experiences into my stash, I'm proud of my
+        <div className="text">
+          <p>
+            My name is Isaac and I'm a work in progress. While I'm still
+            <br /> gathering more experiences into my stash, I'm proud of my
+            <br />
+            skills in C, C++, HTML, CSS &amp; JavaScript
+          </p>
+          <p id="note">Continue scrolling to see what I'm made of</p>
+        </div>
+        <img src={Image1} alt="Winter greens" />
+      </div>
+      <div className="section" id="two">
+        <img src={Image2} alt="Hot air balloon" />
+        <h2>
+          Chemical engineer,
           <br />
-          skills in C, C++, HTML, CSS &amp; JavaScript
+          Artist
+          <br />
+          Gen Y/Z
+        </h2>
+        <h2>Something tells me I'm on the right path.</h2>
+        <img src={Image3} alt="Sun over mountains" />
+        <img src={Image4} alt="Huge trees on earth" />
+        <p>
+          My talent lies within my ability to think critically. My passion is in
+          creating aesthetically pleasing work. Technology sector? Yes please!
         </p>
+        <p id="note">Don't forget to check out my 'Works'</p>
+      </div>
+      <div className="section" id="three">
+        <img src={Image5} alt="Hourglass" />
+        <div>
+          <h2>Facts my friends know about me</h2>
+          <p>
+            Badminton, volleyball, taekwondo, skateboarding, snowboarding, rock
+            climbing; hand crafting, hair cutting; roller coasters, sky diving;
+            &amp;
+            <br /> pet pug called PuiPui (a.k.a Fatty)
+          </p>
+          <p id="note">If I'm reborn, let me be a gymnast. Tqvm</p>
+        </div>
       </div>
     </div>
   );
