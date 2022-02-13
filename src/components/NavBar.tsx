306c402980f1fedf344022bca3f03e8b95158cec
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-14 00:59:00 +0800

    removed extra nav functions and added text shadow

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,25 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
-  // window.onscroll = function () {
-  //   myFunction();
-  // };
-  // var navbar = document.getElementById("navbar");
-  // var sticky = document.querySelector("#navbar");
-  // if (sticky !== null) {
-  //   if (sticky instanceof HTMLElement) var element = sticky.offsetTop;
-  // }
-  // function myFunction() {
-  //   if (navbar !== null) {
-  //     if (window.scrollY >= element) {
-  //       navbar.classList.add("sticky");
-  //       console.log("added");
-  //     } else {
-  //       navbar.classList.remove("sticky");
-  //       console.log("removed");
-  //     }
-  //   }
-  // }
   return (
     <div id="navbar">
       <Link to="/about">About</Link>
