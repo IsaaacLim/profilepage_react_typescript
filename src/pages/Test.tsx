10aa87a8b8484f986a76050b842668c85f4113a0
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-15 19:15:20 +0800

    added comments to Parallax

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -14,18 +14,18 @@ export default function Test() {
   return (
     <div style={{ width: "100%", height: "100%", background: "#253237" }}>
       <Parallax ref={parallax} pages={3}>
-        <ParallaxLayer
+        {/******************* Background *************************************/}
+        <ParallaxLayer // bg 2
           offset={1}
           speed={1}
           style={{ backgroundColor: "#805E73" }}
         />
-        <ParallaxLayer
+        <ParallaxLayer // bg 3
           offset={2}
           speed={1}
           style={{ backgroundColor: "#87BCDE" }}
         />
-
-        <ParallaxLayer
+        <ParallaxLayer // bg 1, placed here to overlap
           offset={0}
           speed={0}
           factor={3}
@@ -34,10 +34,10 @@ export default function Test() {
             backgroundSize: "cover",
           }}
         />
-
+        {/******************* Filler pg2 *************************************/}
         <ParallaxLayer
-          offset={1.3}
-          speed={-0.3}
+          offset={1.3} // pushes it higher on pg 2
+          speed={-0.3} // -ve goes from top down
           style={{ pointerEvents: "none" }}
         >
           <img
@@ -46,7 +46,7 @@ export default function Test() {
             alt="nothing"
           />
         </ParallaxLayer>
-
+        {/* cloud 3, top*/}
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
           <img
             src={url("cloud")}
@@ -59,20 +59,20 @@ export default function Test() {
             alt="nothing"
           />
         </ParallaxLayer>
-
+        {/* cloud 2, bottom */}
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
           <img
             src={url("cloud")}
             style={{ display: "block", width: "20%", marginLeft: "70%" }}
             alt="nothing"
           />
-          <img
+          <img //gets pushed below page view from img element above
             src={url("cloud")}
             style={{ display: "block", width: "20%", marginLeft: "40%" }}
             alt="nothing"
           />
         </ParallaxLayer>
-
+        {/* cloud 1, top */}
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
           <img
             src={url("cloud")}
@@ -85,8 +85,12 @@ export default function Test() {
             alt="nothing"
           />
         </ParallaxLayer>
-
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+        {/******************* Filler pg2 to 3 ********************************/}
+        <ParallaxLayer
+          offset={1.6}
+          speed={-0.1} //top down motion
+          style={{ opacity: 0.4 }}
+        >
           <img
             src={url("cloud")}
             style={{ display: "block", width: "20%", marginLeft: "60%" }}
@@ -103,7 +107,7 @@ export default function Test() {
             alt="nothing"
           />
         </ParallaxLayer>
-
+        {/******************* Filler pg3 *************************************/}
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
           <img
             src={url("cloud")}
@@ -117,7 +121,7 @@ export default function Test() {
           />
         </ParallaxLayer>
 
-        <ParallaxLayer
+        <ParallaxLayer //earth
           offset={2.5}
           speed={-0.4}
           style={{
@@ -130,7 +134,7 @@ export default function Test() {
           <img src={url("earth")} style={{ width: "60%" }} alt="nothing" />
         </ParallaxLayer>
 
-        <ParallaxLayer
+        <ParallaxLayer //clients
           offset={2}
           speed={-0.3}
           style={{
@@ -139,8 +143,8 @@ export default function Test() {
             backgroundImage: url("clients", true),
           }}
         />
-
-        <ParallaxLayer
+        {/******************* Main Content ***********************************/}
+        <ParallaxLayer // Main 1
           offset={0}
           speed={0.1}
           onClick={() => parallax.current.scrollTo(1)}
@@ -152,8 +156,7 @@ export default function Test() {
         >
           <img src={url("server")} style={{ width: "20%" }} alt="nothing" />
         </ParallaxLayer>
-
-        <ParallaxLayer
+        <ParallaxLayer // Main 2
           offset={1}
           speed={0.1}
           onClick={() => parallax.current.scrollTo(2)}
@@ -165,8 +168,7 @@ export default function Test() {
         >
           <img src={url("bash")} style={{ width: "40%" }} alt="nothing" />
         </ParallaxLayer>
-
-        <ParallaxLayer
+        <ParallaxLayer // Main 3
           offset={2}
           speed={-0}
           style={{
