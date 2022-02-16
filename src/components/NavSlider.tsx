e65a56ab788a5607e56e8b4294a9586e02fc834b
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-16 19:20:19 +0800

    exported Slider nav to components

---
+++ src/components/NavSlider.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "react-use-gesture";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./styles.module.css"; // tmp
+
+// interface NavItems {
+//   name: string;
+//   link: string;
+// }
+
+// const Navs: NavItems[] = [
+//   {
+//     name: "About",
+//     link: "/about",
+//   },
+//   {
+//     name: "Works",
+//     link: "/works",
+//   },
+// ];
+
+const left = {
+  bg: `linear-gradient(120deg, #f093fb 0%, #f5576c 100%)`,
+  justifySelf: "end", // pos white circle to right
+  content: "About", // background text
+};
+const right = {
+  bg: `linear-gradient(120deg, #96fbc4 0%, #f9f586 100%)`,
+  justifySelf: "start", // pos white circle to left
+  content: "Works", //background text
+};
+
+const dst = 310;
+
+// const Slider: React.FC<{Inter: NavItems}> = ({ children, Inter }) => {
+const Slider: React.FC = ({ children }) => {
+  let navigate = useNavigate();
+  const [{ x, scale, bg, justifySelf, content }, api] = useSpring(() => ({
+    x: 0, // Render position, default for both left and right
+    scale: 1, // Render size, default for both left and right
+    ...left, // Render with const Left first
+  }));
+  const bind = useDrag(({ active, movement: [x], down }) => {
+    if (!down && (x <= -dst || x >= dst)) {
+      return navigate("/works"); // trigger redirections
+      // return navigate(`${Inter[0].link}`);
+    }
+    api.start({
+      x: !active ? 0 : x < 0 ? (x <= -dst ? -dst : x) : x >= dst ? dst : x, // Cover position, with drag limits
+      scale: active ? 1.1 : 1, // Cover size
+      ...(x < 0 ? left : right), // Drag direction, calls the const
+      // add State here
+      immediate: (name) => active && name === "x",
+    });
+  });
+
+  // White circle animation
+  const avSize = x.to({
+    map: Math.abs,
+    range: [0, dst], // animation start - end range from Cover drag range
+    output: [0.5, 1], // Growth speed
+    extrapolate: "clamp", // Limit max size
+  });
+
+  return (
+    <animated.div
+      {...bind()}
+      className={styles.item}
+      style={{
+        background: bg, // Green / Red background
+      }}
+    >
+      {/* Menu Words */}
+      <animated.div className={styles.text}>{content}</animated.div>
+      {/* White circle */}
+      <animated.div
+        className={styles.av}
+        style={{ scale: avSize, justifySelf }}
+      />
+
+      {/* Cover image */}
+      <animated.div
+        className={styles.fg}
+        style={{
+          x, //enable drag animation
+          scale, // enable scale animation
+        }}
+      >
+        {children}
+        {/*  Display text passed from Slider element */}
+      </animated.div>
+    </animated.div>
+  );
+};
+
+export default Slider;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,86 +1,14 @@
 import React from "react";
-import { useSpring, animated } from "@react-spring/web";
-import { useDrag } from "react-use-gesture";
-import { useNavigate } from "react-router-dom";
+import Slider from "../components/NavSlider";
 
-import styles from "./styles.module.css";
+import styles from "./styles.module.css"; // tmp
 
-const left = {
-  bg: `linear-gradient(120deg, #f093fb 0%, #f5576c 100%)`,
-  justifySelf: "end", // pos white circle to right
-  content: "About", // background text
-};
-const right = {
-  bg: `linear-gradient(120deg, #96fbc4 0%, #f9f586 100%)`,
-  justifySelf: "start", // pos white circle to left
-  content: "Works", //background text
-};
-
-const dst = 310;
-
-const Slider: React.FC = ({ children }) => {
-  let navigate = useNavigate();
-  const [{ x, scale, bg, justifySelf, content }, api] = useSpring(() => ({
-    x: 0, // Render position, default for both left and right
-    scale: 1, // Render size, default for both left and right
-    ...left, // Render with const Left first
-  }));
-  const bind = useDrag(({ active, movement: [x], down }) => {
-    if (!down && (x <= -dst || x >= dst)) {
-      return navigate("/works"); // trigger redirections
-    }
-    api.start({
-      x: !active ? 0 : x < 0 ? (x <= -dst ? -dst : x) : x >= dst ? dst : x, // Cover position, with drag limits
-      scale: active ? 1.1 : 1, // Cover size
-      ...(x < 0 ? left : right), // Drag direction, calls the const
-      // add State here
-      immediate: (name) => active && name === "x",
-    });
-  });
-
-  // White circle animation
-  const avSize = x.to({
-    map: Math.abs,
-    range: [0, dst], // animation start - end range from Cover drag range
-    output: [0.5, 1], // Growth speed
-    extrapolate: "clamp", // Limit max size
-  });
-
-  return (
-    <animated.div
-      {...bind()}
-      className={styles.item}
-      style={{
-        background: bg, // Green / Red background
-      }}
-    >
-      {/* Menu Words */}
-      <animated.div className={styles.text}>{content}</animated.div>
-      {/* White circle */}
-      <animated.div
-        className={styles.av}
-        style={{ scale: avSize, justifySelf }}
-      />
-
-      {/* Cover image */}
-      <animated.div
-        className={styles.fg}
-        style={{
-          x, //enable drag animation
-          scale, // enable scale animation
-        }}
-      >
-        {children}
-        {/*  Display text passed from Slider element */}
-      </animated.div>
-    </animated.div>
-  );
-};
-
+// export default function Test(props: NavItems) {
 export default function Test() {
   return (
     <div className={styles.container}>
-      <Slider>Menu.</Slider>
+      {/* <Slider Inter={Navs}>Slide.</Slider> */}
+      <Slider>Slide.</Slider>
     </div>
   );
 }
