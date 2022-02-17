0fe873a7562a212e4b0eec66a812522d8665b74f
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-18 00:38:49 +0800

    added spring animation to test for socials

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSpring, animated, config } from "@react-spring/web";
+
+function InheritedProps() {
+  const n = useRef(0);
+  const styles = useSpring({
+    from: { x: 0, y: 0 },
+    config: { mass: 10, tension: 180, friction: 12 },
+    to: { x: 100, y: 10 },
+    //loop: {
+    //x: 100,
+    //},
+  });
+
+  return (
+    <animated.div
+      style={{
+        width: 80,
+        height: 80,
+        backgroundColor: "#46e891",
+        borderRadius: 16,
+        ...styles,
+      }}
+    />
+  );
+}
 
 export default function Test() {
   const styles = useSpring({
@@ -19,6 +43,7 @@ export default function Test() {
   return (
     <div>
       <animated.div style={styles}>I will fade in and out</animated.div>
+      <InheritedProps />
     </div>
   );
 }
