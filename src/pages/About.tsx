09028a9dd180f2b35295a15308dbf78afb6b6958
IsaaacLim <isaaclimjj@yahoo.com>
2022-02-24 22:28:40 +0800

    removed gen Z

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -178,7 +178,7 @@ export default function About() {
             <br />
             Artist,
             <br />
-            Gen Y/Z
+            Gen Y
           </h2>
         </ParallaxLayer>
         <ParallaxLayer
