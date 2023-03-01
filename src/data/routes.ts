1ebb2e7d96d2597de00e0c29807b8680a8b9a2da
IsaaacLim <isaaclimjj@gmail.com>
2023-03-01 21:33:06 +0800

    temp: testing react-drag on mobile

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -4,43 +4,48 @@ import AboutPage from "../pages/About";
 import HomePage from "../pages/Home";
 import WorksPage from "../pages/Works";
 import TestPage from "../pages/Test";
+import TestPage2 from "../pages/Test2";
 import Page404 from "../pages/404";
 
 /**
  * Used only in App.tsx
  * To configure available routes
- * 
+ *
  * Didn't combine `routes` with `navLists` because reusing this variable in Pages
  * 	will cause "circular dependencies"
  */
 
-const home = navLists[0]
-const about = navLists[1]
-const works = navLists[2]
-const test = navLists[3]
-const notFound = navLists[4]
+const home = navLists[0];
+const about = navLists[1];
+const works = navLists[2];
+const test = navLists[3];
+const notFound = navLists[4];
 
 const routes: IRoute[] = [
-	{
-		path: home.path,
-		component: HomePage, 
-	},
-	{
-		path: about.path,
-		component: AboutPage,
-	},
-	{
-		path: works.path,
-		component: WorksPage,
-	},
-	{
-		path: test.path,
-		component: TestPage,
-	},
-	{
-		path: notFound.path,
-		component: Page404,
-	}
-]
+  {
+    path: home.path,
+    component: HomePage,
+  },
+  {
+    path: about.path,
+    component: AboutPage,
+  },
+  {
+    path: works.path,
+    component: WorksPage,
+  },
+  {
+    path: test.path,
+    component: TestPage,
+  },
+  {
+    path: notFound.path,
+    component: Page404,
+  },
+  {
+    path: "/test2",
+    component: TestPage2,
+  },
+];
 
 export default routes;
---
+++ src/pages/Test2.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Test3 from "../components/Test3";
+
+const Test2 = () => {
+  return <Test3 />;
+};
+
+export default Test2;
